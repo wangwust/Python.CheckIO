37db982a840cba390c104103d5494f412b42dbcf
wangwust <1277955953@qq.com>
2018-02-22 10:19:54 +0800

    文件夹归类；新增携程机票查询类

--- Python.CheckIO/Other/Faboracci.py
+++ Python.CheckIO/Other/Faboracci.py
@@ -2,7 +2,7 @@
 def fab1(max): 
    n, a, b = 0, 0, 1 
    while n < max: 
-       print b 
+       print(b) 
        a, b = b, a + b 
        n = n + 1
 
---
+++ Python.CheckIO/test.py
@@ -0,0 +1,8 @@
+   
+
+from pprint import pprint
+
+a = input("请输入中文：")
+
+pprint("房间卡萨积分", indent = 1)
+print("房间卡萨积分")
